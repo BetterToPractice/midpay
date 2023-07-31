471f90964415c205525a42f02bda7bf1ec7a4f39
Agung Yuliyanto <agung.96tm@gmail.com>
2023-07-31 13:00:13 +0700

    adjustment crawls and scripts, remove index_url

--- src/midpay/settings.py
+++ src/midpay/settings.py
@@ -6,17 +6,14 @@ VA_NAME_CHOICES = [BCA_VA, BNI_VA, PERMATA_VA]
 
 VA_LINKS = {
     BCA_VA: {
-        'index_url': 'https://simulator.sandbox.midtrans.com/bca/va/index',
         'inquiry_url': 'https://simulator.sandbox.midtrans.com/bca/va/inquiry',
         'payment_url': 'https://simulator.sandbox.midtrans.com/bca/va/payment',
     },
     BNI_VA: {
-        'index_url': 'https://simulator.sandbox.midtrans.com/bni/va/index',
         'inquiry_url': 'https://simulator.sandbox.midtrans.com/bni/va/inquiry',
         'payment_url': 'https://simulator.sandbox.midtrans.com/bni/va/payment',
     },
     PERMATA_VA: {
-        'index_url': 'https://simulator.sandbox.midtrans.com/permata/va/index',
         'inquiry_url': 'https://simulator.sandbox.midtrans.com/permata/va/inquiry',
         'payment_url': 'https://simulator.sandbox.midtrans.com/permata/va/payment',
     }
